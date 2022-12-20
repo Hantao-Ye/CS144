0b6ec521fe79f23e406c482fb80b9291e21bf5b0
Hantao-Ye <hantaoye2000@gmail.com>
2022-12-19 18:34:11 -0800

    finish lab2 warm up

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -6,7 +6,7 @@
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -14,8 +14,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t isn_raw = static_cast<uint64_t>(isn.raw_value());
+
+    return WrappingInt32{static_cast<uint32_t>((n + isn_raw) % (1UL << 32))};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,33 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t n_abs = static_cast<uint64_t>(n.raw_value() - isn.raw_value());
+    if (n_abs == checkpoint)
+        return n_abs;
+
+    uint64_t left = 0;
+    uint64_t right = UINT32_MAX;
+    while (left <= right) {
+        uint64_t mid = left + (right - left) / 2;
+        uint64_t temp_n_abs = n_abs + mid * (1UL << 32);
+
+        if (temp_n_abs == checkpoint) {
+            return temp_n_abs;
+        } else if (temp_n_abs < checkpoint) {
+            if (mid == UINT32_MAX) {
+                return n_abs + UINT32_MAX * (1UL << 32);
+            }
+            left = mid + 1;
+        } else {
+            if (mid == 0) {
+                return n_abs;
+            }
+            right = mid - 1;
+        }
+    }
+
+    return max(n_abs + left * (1UL << 32), checkpoint) - min(n_abs + left * (1UL << 32), checkpoint) <
+                   max(n_abs + right * (1UL << 32), checkpoint) - min(n_abs + right * (1UL << 32), checkpoint)
+               ? n_abs + left * (1UL << 32)
+               : n_abs + right * (1UL << 32);
 }
